1df0f5ca044f81fdb940d3efc90d13a86ca01352
s22012 <s22012@std.it-college.ac.jp>
2024-02-08 16:25:24 +0900

    Helloまで

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from 'express';
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import cors from 'cors';
+import passport from "passport";
+import passportConfig from "./util/auth.js";
+import session from "express-session";
+// const path = require('path');
+// const cookieParser = require('cookie-parser');
+// const logger = require('morgan');
+import indexRouter from "./routes/index.js";
+import userRouter from "./routes/users.js";
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+// const indexRouter = require('./routes/index');
+// const usersRouter = require('./routes/users');
 
+const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(import.meta.dirname, 'public')));
+app.use(cors());
+app.use("/axios", express.static(path.join(
+    import.meta.dirname, "node_modules", "axios", "dist"
+)));
+// session
+app.use(cookieParser());
+app.use(session({
+    secret: "RWmzrePkh3mkA3oFHmR+JCpl/8lDolml39sCVH/44aiKsNC0",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {maxAge: 60 * 60 * 1000}
+}));
+app.use(passport.authenticate("session"));
+app.use(passportConfig(passport));
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', userRouter);
 
-module.exports = app;
+
+export default app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,81 @@
-var express = require('express');
-var router = express.Router();
+import express from "express";
+import passport from "passport";
+import {check, validationResult} from "express-validator";
+import {calcHash, generateSalt} from "../util/auth.js";
+import {PrismaClient} from "@prisma/client";
+
+const router = express.Router();
+const prisma = new PrismaClient();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", (req, res, next) => {
+
 });
 
-module.exports = router;
+/**
+ * ログイン画面(表示)
+ */
+router.get("/login", (req, res, next) => {
+  const data = {
+    title: "ログイン"
+  };
+  res.render("users/login", data);
+});
+
+/**
+ * ログイン認証
+ */
+router.post("/login", passport.authenticate("local", {
+  failWithError: true
+}),(req, res, next) => {
+  res.json({
+    message: "OK"
+  });
+});
+
+/**
+ * ユーザ新規登録(表示)
+ */
+router.get("/signup", (req, res, next) => {
+  const data = {
+    title: "ユーザ新規登録",
+    name: "",
+    errors: []
+  };
+  res.render("users/signup", data);
+});
+
+/**
+ * ユーザ新規登録処理
+ */
+router.post("/signup", [
+  check("name", "名前の入力は必須です").notEmpty(),
+  check("pass", "パスワードの入力は必須です").notEmpty(),
+], async (req, res, next) => {
+  const result = validationResult(req);
+  if (!result.isEmpty()) {
+    // 入力チェックでだめだった場合
+    const errors = result.array();
+    const data = {
+      title: "ユーザ新規登録",
+      name: req.body.name,
+      errors,
+    };
+    res.render("users/signup", data);
+    return;
+  }
+  // 入力チェックOKだったらこの先に進む。
+  const {name, pass} = req.body;
+  const salt = generateSalt();
+  const hashedPassword = calcHash(pass, salt);
+  await prisma.user.create({
+    data: {
+      name,
+      pass: hashedPassword,
+      salt
+    }
+  });
+  res.redirect("/users/login");
+});
+
+export default router;
